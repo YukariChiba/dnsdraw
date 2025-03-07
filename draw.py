887ce4d007481fa13a7be97fc6e58fd403bd4133
Yukari Chiba <i@0x7f.cc>
2025-03-07 10:52:17 +0800

    response ICMPv6DestUnreach for target ip

--- draw.py
+++ draw.py
@@ -1,6 +1,6 @@
 import sys
 from netfilterqueue import NetfilterQueue
-from scapy.all import IPv6, ICMPv6TimeExceeded
+from scapy.all import IPv6, ICMPv6TimeExceeded, ICMPv6DestUnreach
 from scapy.sendrecv import __gen_send  # `send` is fucking slow!
 from scapy.config import conf
 from ipaddress import IPv6Address
@@ -19,10 +19,15 @@ def handle(inpkt):
     group = (int(dstaddr)-int(baseaddr))>>8
     set_len = (int(dstaddr) & 0xff)
     hlim = pkt.hlim
-    if hlim < MAX_LEN and hlim - 1 <= set_len:
+    if hlim < MAX_LEN and hlim - 1 < set_len:
         inpkt.drop()
         sendsrc = baseaddr + (group<<8) + hlim - 1
         tosend = IPv6(src=str(sendsrc), dst=pkt.src) / ICMPv6TimeExceeded() / pkt
+        __gen_send(socket, tosend)
+    elif hlim - 1 == set_len:
+        inpkt.drop()
+        sendsrc = baseaddr + (group<<8) + hlim - 1
+        tosend = IPv6(src=str(sendsrc), dst=pkt.src) / ICMPv6DestUnreach() / pkt
         __gen_send(socket, tosend)
     else:
         inpkt.drop()
